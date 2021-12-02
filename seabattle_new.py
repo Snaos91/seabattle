bcb24ecedb2ce4d317f7c2b77015273cedfb8657
Snaos91 <armsnaos@yandex.ru>
2021-12-02 20:08:33 +0300

    Final
    Final

--- seabattle_new.py
+++ seabattle_new.py
@@ -1,5 +1,4 @@
 from random import randint
-import snoop
 
 
 class ShipException(Exception):
@@ -41,6 +40,9 @@ class Board:
 
         self.busy = []
         self.list_ships = []
+        self.ships = []
+        self.busy_game = []
+        self.count = 0
 
     def __str__(self):
         res = ""
@@ -62,8 +64,11 @@ class Board:
             self.busy.append(d)
             self.list_ships.append(d)
 
-    def add_contour(self):
-        for j in self.list_ships:
+        self.ships.append(ship)
+
+    def add_contour_placement(self):
+        lst_ship = self.list_ships
+        for j in lst_ship:
             try:
                 if self.field[j[0] - 1][j[1] - 1] == "-":
                     self.field[j[0] - 1][j[1] - 1] = "*"
@@ -121,19 +126,114 @@ class Board:
         except IndexError:
             pass
 
+    def shot(self, d):
+        if d in self.busy_game:
+            raise ShipException
+
+        self.busy_game.append(d)
+
+        for ship in self.ships:
+            if d in ship.dots:
+                ship.lives -= 1
+                self.field[d[0]][d[1]] = "X"
+                if ship.lives == 0:
+                    self.count += 1
+                    print("Корабль уничтожен!")
+                    for j in self.busy_game:
+                        if self.field[j[0]][j[1]] == "X":
+                            try:
+                                if self.field[j[0] - 1][j[1] - 1] == "-":
+                                    self.field[j[0] - 1][j[1] - 1] = "*"
+                                    self.busy_game.append([j[0] - 1, j[1] - 1])
+                            except IndexError:
+                                pass
+                            try:
+                                if self.field[j[0]][j[1] - 1] == "-":
+                                    self.field[j[0]][j[1] - 1] = "*"
+                                    self.busy_game.append([j[0], j[1] - 1])
+                            except IndexError:
+                                pass
+                            try:
+                                if self.field[j[0] + 1][j[1] - 1] == "-":
+                                    self.field[j[0] + 1][j[1] - 1] = "*"
+                                    self.busy_game.append([j[0] + 1, j[1] - 1])
+                            except IndexError:
+                                pass
+                            try:
+                                if self.field[j[0] + 1][j[1]] == "-":
+                                    self.field[j[0] + 1][j[1]] = "*"
+                                    self.busy_game.append([j[0] + 1, j[1]])
+                            except IndexError:
+                                pass
+                            try:
+                                if self.field[j[0] + 1][j[1] + 1] == "-":
+                                    self.field[j[0] + 1][j[1] + 1] = "*"
+                                    self.busy_game.append([j[0] + 1, j[1] + 1])
+                            except IndexError:
+                                pass
+                            try:
+                                if self.field[j[0]][j[1] + 1] == "-":
+                                    self.field[j[0]][j[1] + 1] = "*"
+                                    self.busy_game.append([j[0], j[1] + 1])
+                            except IndexError:
+                                pass
+                            try:
+                                if self.field[j[0] - 1][j[1] + 1] == "-":
+                                    self.field[j[0] - 1][j[1] + 1] = "*"
+                                    self.busy_game.append([j[0] - 1, j[1] + 1])
+                            except IndexError:
+                                pass
+                            try:
+                                if self.field[j[0] - 1][j[1]] == "-":
+                                    self.field[j[0] - 1][j[1]] = "*"
+                                    self.busy_game.append([j[0] - 1, j[1]])
+                            except IndexError:
+                                pass
+                    return False
+                else:
+                    print("Корабль ранен!")
+                    return True
+
+        self.field[d[0]][d[1]] = "*"
+        print("Мимо!")
+        return False
+
 
 class Player:
     def __init__(self, board, enemy):
         self.board = board
         self.enemy = enemy
 
+    def move(self):
+        while True:
+            try:
+                target = self.input_motion()
+                repeat = self.enemy.shot(target)
+                return repeat
+            except ShipException as e:
+                print(e)
+
 
 class AI(Player):
-    pass
+    def input_motion(self):
+        d = [randint(1, 6), randint(1, 6)]
+        print(f"Ход компьютера: {d[0]} {d[1]}")
+        return d
 
 
 class User(Player):
-    pass
+    def input_motion(self):
+        while True:
+            try:
+                player_line = int(input("Выберите линию для выстрела...."))
+                player_column = int(input("Выберите столбец для выстрела... "))
+                if 1 <= player_line <= 6 and 1 <= player_column <= 6:
+                    return [player_line, player_column]
+                else:
+                    raise ValueError
+            except ValueError:
+                print("Ошибка! Попробуйте снова")
+
 
 class Game:
     def __init__(self):
@@ -162,7 +262,7 @@ class Game:
                 ship = Ship(i, [randint(1, 6), randint(1, 6)], randint(1, 2), i)
                 try:
                     brd.add_ships(ship)
-                    brd.add_contour()
+                    brd.add_contour_placement()
                     break
                 except ShipException:
                     pass
@@ -170,16 +270,46 @@ class Game:
         return brd
 
     def loop(self):
-        print("\n")
-        print("Доска пользователя")
-        print(self.us.board)
-        print("-----------------")
-        print("Доска компьютера")
-        print(self.ai.board)
+        num = 0
+        while True:
+            print("-" * 20)
+            print("Доска пользователя:")
+            print(self.us.board)
+            print("-" * 20)
+            print("Доска компьютера:")
+            print(self.ai.board)
+            if num % 2 == 0:
+                print("-" * 20)
+                print("Ходит пользователь!")
+                repeat = self.us.move()
+            else:
+                print("-" * 20)
+                print("Ходит компьютер!")
+                repeat = self.ai.move()
+            if repeat:
+                num -= 1
+
+            if self.ai.board.count == 7:
+                print("-" * 20)
+                print("Пользователь выиграл!")
+                break
+
+            if self.us.board.count == 7:
+                print("-" * 20)
+                print("Компьютер выиграл!")
+                break
+            num += 1
+
+    def greet(self):
+        print("-------------------")
+        print("  Приветсвуем вас  ")
+        print("      в игре       ")
+        print("    морской бой    ")
 
     def start(self):
+        self.greet()
         self.loop()
 
 
 game = Game()
-game.loop()
+game.start()
